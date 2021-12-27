0b144c7767ccc80668168c9477efa90b4c2ef070
Vlad Dorokh <vlad.doroh@gmail.com>
2021-12-27 18:40:42 +0300

    Added Medicines controller.

--- MedicineManaging.API/Controllers/MedicinesController.cs
+++ MedicineManaging.API/Controllers/MedicinesController.cs
@@ -1,4 +1,7 @@
 using MediatR;
+using MedicineManaging.Domain.Entities.Medicines;
+using MedicineManaging.Infrastructure.MediatR.Commands;
+using MedicineManaging.Infrastructure.MediatR.Queries;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicineManaging.API.Controllers
@@ -14,6 +17,46 @@ namespace MedicineManaging.API.Controllers
         {
             _logger = logger;
             _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMedicineByIdAsync(string id)
+        {
+            var medicine = await _mediator.Send(new GetMedicineByIdQuery(id));
+
+            return Ok(medicine);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMedicinesAsync()
+        {
+            var medicines = await _mediator.Send(new GetMedicinesQuery());
+
+            return Ok(medicines);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMedicineAsync(Medicine medicine)
+        {
+            await _mediator.Send(new AddMedicineCommand(medicine));
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMedicineAsync(string id, Medicine medicine)
+        {
+            await _mediator.Send(new UpdateMedicineCommand(id, medicine));
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMedicineAsync(string id)
+        {
+            await _mediator.Send(new DeleteMedicineByIdCommand(id));
+
+            return Ok();
         }
     }
 }
